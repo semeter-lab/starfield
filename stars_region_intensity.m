e7a5be787d8177f3f62fb7d107da623f8ced93f8
scivision <scivision@users.noreply.github.com>
2023-06-28 14:51:34 -0400

    two-plots

--- stars_region_intensity.m
+++ stars_region_intensity.m
@@ -29,7 +29,7 @@ t = tiledlayout(2, 2, parent=f1);
 title(t, fn, interpreter="none")
 
 % original image
-ax1 = nexttile(t);
+ax1 = nexttile(t, [2, 1]);
 imagesc(ax1, gr)
 c = colorbar(ax1);
 ylabel(c, "intensity")
@@ -38,30 +38,44 @@ title(ax1, "original image:")
 ylabel(ax1, "y")
 xlabel(ax1, "x")
 
-% image region averages
-ax2 = nexttile(t);
-imagesc(ax2, I)
-c = colorbar(ax2);
-colormap(ax2, "bone")
-ylabel(c, "average intensity")
-ylabel(ax2, "y-region")
-xlabel(ax2, "x-region")
-title(ax2, "average region intensity")
-
 % collapsed intensity (average over columns and rows respectively)
 Iy = sum(gr, 2) / width;
 Ix = sum(gr, 1) / height;
 
-ax3 = nexttile(t);
-plot(ax3, Iy, 1:height, Color="red")
-set(ax3, YDir="reverse", ylim=[1, height])
-ylabel(ax3, "y")
-xlabel(ax3, "row intensity average")
-title(ax3, "average row intensity")
+ax2 = nexttile(t);
+plot(ax2, Iy, 1:height, Color="red")
+set(ax2, YDir="reverse", ylim=[1, height])
+ylabel(ax2, "y")
+xlabel(ax2, "row intensity average")
+title(ax2, "average row intensity")
 
-ax4 = nexttile(t);
-plot(ax4, 1:width, Ix, Color="blue")
-set(ax4, XLim=[1, width])
-xlabel(ax4, "x")
-ylabel(ax4, "column intensity average")
-title(ax4, "average column intensity")
+ax3 = nexttile(t);
+plot(ax3, 1:width, Ix, Color="blue")
+set(ax3, XLim=[1, width])
+xlabel(ax3, "x")
+ylabel(ax3, "column intensity average")
+title(ax3, "average column intensity")
+
+% region plots
+f2 = figure(2);
+clf(f2)
+
+t = tiledlayout(2, 1);
+title(t, fn, interpreter="none")
+
+% image region averages
+ax1 = nexttile(t);
+imagesc(ax1, I)
+c = colorbar(ax1);
+colormap(ax1, "bone")
+ylabel(c, "average intensity")
+ylabel(ax1, "y-region")
+xlabel(ax1, "x-region")
+title(ax1, "average region intensity")
+
+% histogram of regions
+ax2 = nexttile(t);
+histogram(ax2, I)
+ylabel(ax2, "Number of regions")
+xlabel(ax2, "Intensity bin")
+title(ax2, "region histogram")
